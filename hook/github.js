99699518a13cc97afbc2d462c17b2cd902473c40
maxime blanc <maximeblanc.dev@gmail.com>
2018-05-22 17:39:46 +0200

    can update table with parrot comment

--- hook/github.js
+++ hook/github.js
@@ -2,8 +2,9 @@ const models = require("./models");
 const db = require("./db");
 const { mapGithubAccountToUserId } = require("./utils/users");
 const zenhubFetch = require("./utils/zenhub");
+const { getEstimation, getStatus } = require("./utils/selector");
 
-const githubHook = ({ action, issue }) => {
+const githubHook = ({ action, issue, comment }) => {
   let haveMatch = true;
 
   switch (action) {
@@ -42,24 +43,73 @@ const githubHook = ({ action, issue }) => {
       break;
     }
 
+    case "created": {
+      const { number, title, assignee, html_url } = issue;
+      const userId = mapGithubAccountToUserId(assignee.login);
+
+      if (comment.body.includes("parrot")) {
+        models.Task.findAll({
+          where: {
+            numero: number
+          }
+        }).then(data => {
+          if (data.length === 0) {
+            models.Task.create({
+              libelle: title,
+              pc: null,
+              id_us: null,
+              id_sprint: null,
+              url: html_url,
+              id_type_tache: null,
+              id_utilisateur: userId,
+              status: null,
+              numero: number
+            });
+
+            return;
+          }
+
+          zenhubFetch(`/issues/${number}`).then(({ data }) => {
+            models.Task.update(
+              {
+                status: getStatus(data),
+                pc: getEstimation(data)
+              },
+              {
+                where: {
+                  number: number
+                }
+              }
+            );
+          });
+        });
+      }
+
+      haveMatch = false;
+      break;
+    }
+
     default:
       haveMatch = false;
       break;
   }
 
-  //   if (haveMatch) {
-  //     const { number } = issue;
-  //     zenhubFetch(`/issues/${number}`).then(({ data }) => {
-  //       models.Task.update(
-  //         {},
-  //         {
-  //           where: {
-  //             number: number
-  //           }
-  //         }
-  //       );
-  //     });
-  //   }
+  if (haveMatch) {
+    const { number } = issue;
+    zenhubFetch(`/issues/${number}`).then(({ data }) => {
+      models.Task.update(
+        {
+          status: getStatus(data),
+          pc: getEstimation(data)
+        },
+        {
+          where: {
+            number: number
+          }
+        }
+      );
+    });
+  }
 };
 
 module.exports = githubHook;
--- hook/server.js
+++ hook/server.js
@@ -13,18 +13,12 @@ app.use(bodyParser.json());
 
 app.post("/github", (req, res) => {
   github(req.body);
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.post("/zenhub", (req, res) => {
   zenhub(req);
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-const fetch = require("./utils/zenhub");
-
-fetch.get("/issues/123").then(({ data }) => {
-  console.log(data);
-});
