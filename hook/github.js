2573ec47964ec6dc4f7dddad2614432f2c9b1bae
maxime blanc <maximeblanc.dev@gmail.com>
2018-05-28 17:55:33 +0200

    parrot work

--- hook/github.js
+++ hook/github.js
@@ -3,6 +3,7 @@ const db = require("./db");
 const { mapGithubAccountToUserId, getUsersId } = require("./utils/users");
 const zenhubFetch = require("./utils/zenhub");
 const { getEstimation, getStatus } = require("./utils/selector");
+const { mapLabelsToLabelId, getSprintNumber } = require("./utils/github");
 
 const githubHook = ({ action, issue, comment }) => {
   let haveMatch = true;
@@ -26,6 +27,61 @@ const githubHook = ({ action, issue, comment }) => {
       break;
     }
 
+    case "created": {
+      const { number, title, assignees, html_url, labels, milestone } = issue;
+
+      const usersId = getUsersId(assignees);
+
+      if (comment.body.includes("parrot")) {
+        models.Task.findAll({
+          where: {
+            numero: number
+          }
+        }).then(data => {
+          if (data.length === 0) {
+            zenhubFetch(`/issues/${number}`).then(({ data }) => {
+              models.Task.create({
+                libelle: title,
+                status: getStatus(data),
+                pc: getEstimation(data),
+                id_us: null,
+                id_sprint: getSprintNumber(milestone),
+                url: html_url,
+                id_type_tache: mapLabelsToLabelId(labels),
+                id_utilisateur: usersId,
+                numero: number
+              });
+            });
+
+            return;
+          }
+
+          zenhubFetch(`/issues/${number}`).then(({ data }) => {
+            models.Task.update(
+              {
+                libelle: title,
+                status: getStatus(data),
+                pc: getEstimation(data),
+                id_sprint: getSprintNumber(milestone),
+                url: html_url,
+                id_type_tache: mapLabelsToLabelId(labels),
+                id_utilisateur: usersId,
+                numero: number
+              },
+              {
+                where: {
+                  numero: number
+                }
+              }
+            );
+          });
+        });
+      }
+
+      haveMatch = false;
+      break;
+    }
+
     case "assigned": {
       const { number, assignees } = issue;
       const usersId = getUsersId(assignees);
@@ -40,59 +96,6 @@ const githubHook = ({ action, issue, comment }) => {
           }
         }
       );
-      break;
-    }
-
-    case "created": {
-      const { number, title, assignees, html_url } = issue;
-
-      const usersId = getUsersId(assignees);
-
-      console.log(usersId);
-
-      if (comment.body.includes("parrot")) {
-        models.Task.findAll({
-          where: {
-            numero: number
-          }
-        }).then(data => {
-          if (data.length === 0) {
-            models.Task.create({
-              libelle: title,
-              pc: null,
-              id_us: null,
-              id_sprint: null,
-              url: html_url,
-              id_type_tache: null,
-              id_utilisateur: usersId,
-              status: null,
-              numero: number
-            });
-
-            return;
-          }
-
-          zenhubFetch(`/issues/${number}`).then(({ data }) => {
-            models.Task.update(
-              {
-                libelle: title,
-                status: getStatus(data),
-                pc: getEstimation(data),
-                id_utilisateur: usersId,
-                url: html_url,
-                numero: number
-              },
-              {
-                where: {
-                  numero: number
-                }
-              }
-            );
-          });
-        });
-      }
-
-      haveMatch = false;
       break;
     }
 
